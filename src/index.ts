29d156a876912802ccf0b1ced96a1da7f6550458
Leozindev <leonardo16almeida@gmail.com>
2025-04-22 16:42:47 -0300

    FEAT: Update Fastify server initialization to use environment variables for host and port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import FastifyServer from "./server/fastify";
 
 const routes = [...serverRoutes];
 
-const server = new FastifyServer(3000, "localhost");
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+const server = new FastifyServer(PORT, HOST);
 server.registerRoutes(routes);
 
 server.start();
